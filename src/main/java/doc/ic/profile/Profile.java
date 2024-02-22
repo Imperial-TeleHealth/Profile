3f29d37d5edcc8cad28aba097a6e2c31616952cb
Daniel Huang <74921405+daniel7722@users.noreply.github.com>
2024-02-22 23:19:00 +0000

    feat: added project structure Constructed Springboot environment with maven build

---
+++ src/main/java/doc/ic/profile/Profile.java
@@ -0,0 +1,12 @@
+package doc.ic.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Profile {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Profile.class, args);
+  }
+}

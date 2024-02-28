b24601bc3da8cc80d444e58c02ced25d754a921c
Daniel Huang <74921405+daniel7722@users.noreply.github.com>
2024-02-28 16:37:41 +0000

    test (DH): created test to test addCustomer and deleteCustomer

--- src/test/java/CustomerServiceTest.java
+++ src/test/java/CustomerServiceTest.java
@@ -1,15 +1,18 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.Matchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
 import doc.ic.profile.Customer;
 import doc.ic.profile.CustomerPasswordRepository;
 import doc.ic.profile.CustomerRepository;
 import doc.ic.profile.CustomerService;
+import doc.ic.profile.NewCustomerRequest;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
@@ -23,16 +26,38 @@ public class CustomerServiceTest {
   private final CustomerPasswordRepository customerPasswordRepository = mock(CustomerPasswordRepository.class);
 
   private final List<Customer> customersList= new ArrayList<>();
+  private final CustomerService customerService = new CustomerService(customerRepository, customerPasswordRepository);
+
+  private final Customer customer = new Customer("username", "name", "email", 20);
 
   @Test
   public void getCustomerReturnsListOfCustomers() {
     // Arrange
-    customersList.add(new Customer("username", "name", "email", 20));
-    CustomerService customerService = new CustomerService(customerRepository, customerPasswordRepository);
+    customersList.add(customer);
     when(customerRepository.findAll()).thenReturn(customersList);
     // Act
     List<Customer> customers = customerService.getCustomer();
     // Assert
-    assertEquals(1, customers.size());
+    Assertions.assertEquals(1, customers.size());
+  }
+
+  @Test
+  public void addCustomerSavesCustomers() {
+    // Arrange
+    NewCustomerRequest request = new NewCustomerRequest("username", "name", "email", 20);
+    // Act
+    customerService.addCustomer(request);
+    // Assert
+    Customer save = Mockito.verify(customerRepository, times(1)).save(customer);
+  }
+
+  @Test
+  public void deleteCustomerDeletesCustomers() {
+    // Arrange
+    String username = "username";
+    // Act
+    customerService.deleteCustomer(username);
+    // Assert
+    Mockito.verify(customerRepository, times(1)).deleteById(username);
   }
 }
